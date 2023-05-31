96bb4267851bb9964ce230653cdb8e52109c2b83
moisesfdasilva <msesfernandes@gmail.com>
2023-05-31 19:29:40 -0300

    feat: criada a entidade Serie, em model.

--- src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Serie.java
@@ -1,9 +1,46 @@
 package com.trybe.acc.java.minhasseries.model;
 
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Serie {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String nome;
+  private List<Episodio> episodios;
+
+  public Serie(String nome) {
+    this.nome = nome;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Episodio> GetEpisodios() {
+    return episodios;
+  }
+
+  public void SetEpisodios(List<Episodio> episodios) {
+    this.episodios = episodios;
+  }
 
 }
