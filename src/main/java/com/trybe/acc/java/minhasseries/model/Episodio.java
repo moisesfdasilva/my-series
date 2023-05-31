78d95562aa2962db55694aae7ee6ca32e7cf2342
moisesfdasilva <msesfernandes@gmail.com>
2023-05-31 19:20:42 -0300

    feat: criada a entidade Episodio, em model.

--- src/main/java/com/trybe/acc/java/minhasseries/model/Episodio.java
+++ src/main/java/com/trybe/acc/java/minhasseries/model/Episodio.java
@@ -1,9 +1,47 @@
 package com.trybe.acc.java.minhasseries.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Episodio {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private Integer numero;
+  private Integer duracaoEmMinutos;
+
+  public Episodio(Long id, Integer numero, Integer duracaoEmMinutos) {
+    this.id = id;
+    this.numero = numero;
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Integer getNumero() {
+    return numero;
+  }
+
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
+
+  public Integer getDuracaoEmMinutos() {
+    return duracaoEmMinutos;
+  }
+
+  public void setDuracaoEmMinutos(Integer duracaoEmMinutos) {
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
 
 }
